d71992feaa91f65b39c35a5eaa191f580d46ee4c
Rock Lee <466186888@qq.com>
2018-11-15 11:11:46 +0800

    init

---
+++ tensquare_common/src/main/java/entity/PageResult.java
@@ -0,0 +1,36 @@
+package entity;
+
+import java.util.List;
+
+/**
+ * @Author Rock Lee
+ * @Date 2018/11/15 0015 10:55
+ */
+public class PageResult<T> {
+    private long total;
+    private List<T> rows;
+
+    public PageResult() {
+    }
+
+    public PageResult(long total, List<T> rows) {
+        this.total = total;
+        this.rows = rows;
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
+    public List<T> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<T> rows) {
+        this.rows = rows;
+    }
+}
