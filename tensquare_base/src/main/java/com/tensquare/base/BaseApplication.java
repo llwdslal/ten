9e1cc32c5a1fbf7d9fa3bc68e66123c460c7667d
Rock Lee <466186888@qq.com>
2018-11-16 14:14:50 +0800

    完成 标签模块

--- tensquare_base/src/main/java/com/tensquare/base/BaseApplication.java
+++ tensquare_base/src/main/java/com/tensquare/base/BaseApplication.java
@@ -2,6 +2,8 @@ package com.tensquare.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import util.IdWorker;
 
 /**
  * @Author Rock Lee
@@ -11,5 +13,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(BaseApplication.class,args);
+    }
+
+    @Bean
+    public IdWorker idWorker(){
+        return new IdWorker(1,1);
     }
 }
--- tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
+++ tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
@@ -2,10 +2,14 @@ package com.tensquare.base.controller;
 
 import com.tensquare.base.pojo.Label;
 import com.tensquare.base.service.LabelService;
+import entity.PageResult;
 import entity.Result;
 import entity.StatusCode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @Author Rock Lee
@@ -46,5 +50,17 @@ public class LabelController {
     public Result delete(@PathVariable("labelId") String labelId){
         labelService.deleteById(labelId);
         return new Result(true,StatusCode.OK,"删除成功");
+    }
+
+    @PostMapping("/search")
+    public Result findSearch(@RequestBody Label label){
+        List<Label> list = labelService.findSearch(label);
+        return new Result(true,StatusCode.OK,"查询成功",list);
+    }
+
+    @PostMapping("/search/{page}/{size}")
+    public Result pageQuery(@RequestBody Label label,@PathVariable int page,@PathVariable int size){
+        Page<Label> list = labelService.pageQuery(label,page,size);
+        return new Result(true,StatusCode.OK,"查询成功",new PageResult<Label>(list.getTotalElements(),list.getContent()));
     }
 }
